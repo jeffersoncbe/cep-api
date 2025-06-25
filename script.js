b85fe838390c7abf9e66bcb025f17a937b227408
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-06-24 22:06:48 -0300

    Inserindo API de Estado

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ const consultarCep = () => {
     console.log(`URI: ${uri}`)
     fetch(uri)
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+            console.log(json)
+
+            document.getElementById('logradouro').value = json.address
+            document.getElementById('bairro').value = json.district
+            document.getElementById('localidade').value = json.city
+            document.getElementById('uf').value = json.state
+            document.getElementById('ddd').value = json.ddd
+
+        
+        })
 }
